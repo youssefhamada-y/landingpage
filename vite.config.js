9efcbdd6838d16504919cdccd08a662edacb61c1
Youssef hamada <yhamada504@gmail.com>
2025-05-04 13:24:18 +0300

    شرح التعديلات

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/landingpage/',  
   plugins: [
-    base: '/landingpage/',  
     react(),
     tailwindcss()
   ],
